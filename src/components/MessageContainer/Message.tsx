56f84d791615e4247c5b518e75870d63efb57ed2
abg3000 <abg3000@naver.com>
2022-02-12 11:12:45 +0900

    [Fix] 중간 대호 삭제시 모달유지 버그 수정
    - deleteHandler를 props로 내려주는 방식이 아닌 직접 redux로 관리

--- src/components/MessageContainer/Message.tsx
+++ src/components/MessageContainer/Message.tsx
@@ -1,20 +1,22 @@
 import { useState } from 'react';
+import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import { message } from 'types/message';
 import { shortening } from 'utils/functions/shortening';
 import { RootState } from 'redux/reducer';
 import { reduxUser } from 'types/reduxTypes';
+import { deleteMessage, deleteMessageAction } from 'redux/actions/chatAction';
 import './Message.scss';
 
 interface reduxProps {
   message: message;
   user: reduxUser;
-  deleteHandler: (message: message) => (e: React.MouseEvent<HTMLButtonElement>) => void;
   setMessage: React.Dispatch<React.SetStateAction<string>>;
+  deleteMessage: (message: message) => deleteMessageAction;
   inputMessage: string;
 }
 
-function Message({ message, user, deleteHandler, setMessage, inputMessage }: reduxProps) {
+function Message({ message, user, deleteMessage, setMessage, inputMessage }: reduxProps) {
   const [isMouseOver, setIsMouseOver] = useState(false);
   const [isDelete, setIsDelete] = useState(false);
   const content = message.content.split('\n');
@@ -29,8 +31,8 @@ function Message({ message, user, deleteHandler, setMessage, inputMessage }: red
     setIsMouseOver(false);
   };
 
-  const messageDeleteHandler = () => {
-    deleteHandler(message);
+  const messageDeleteHandler = (message: message) => () => {
+    deleteMessage(message);
     isDeleteHandler();
     hideFloatMenu();
   };
@@ -85,7 +87,7 @@ function Message({ message, user, deleteHandler, setMessage, inputMessage }: red
               </div>
             </div>
             <div className='button-wrap'>
-              <button onClick={deleteHandler(message)}>삭제하기</button>
+              <button onClick={messageDeleteHandler(message)}>삭제하기</button>
               <button onClick={isDeleteHandler} className='nobutton'>
                 취소
               </button>
@@ -97,6 +99,11 @@ function Message({ message, user, deleteHandler, setMessage, inputMessage }: red
   );
 }
 
-export default connect((state: RootState) => ({
-  user: state.userReducer.isLoggin,
-}))(Message);
+export default connect(
+  (state: RootState) => ({
+    user: state.userReducer.isLoggin,
+  }),
+  (dispatch: Dispatch) => ({
+    deleteMessage: (message: message) => dispatch(deleteMessage(message)),
+  })
+)(Message);
